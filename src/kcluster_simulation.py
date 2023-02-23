e2fa5cfc2f34b5b94b96b64dad545875715c326d
Iker Olarra <iker.mt@gmail.com>
2023-02-23 11:24:30 +0100

    Wrote a function to estimate cost based on a given solution consisting of location of stations, location of evs and the assignment between one and the other

--- src/kcluster_simulation.py
+++ src/kcluster_simulation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from utils import get_ranges, compute_visiting_probability, calculate_distance, estimate_number_of_chargers
+from utils import compute_cost_per_station
 from sklearn.cluster import KMeans
 
 
@@ -15,60 +15,21 @@ maintenance_fee_per_charger = 500
 
 def main():
 
-    cars = pd.read_csv("data/MOPTA2023_car_locations.csv", names=['x', 'y'])
+    cars = pd.read_csv("data/MOPTA2023_car_locations.csv", names=['ev_x', 'ev_y'])
     cars = cars.loc[cars.index.repeat(10)] # Repeat each location 10 times
     cars['loc_ix'] = cars.index
     cars = cars.reset_index(drop=True)
 
-    kmeans = KMeans(n_clusters=600, random_state=0).fit(cars[['x', 'y']])
-    station_location_ixs = kmeans.predict(cars[['x', 'y']])
+    kmeans = KMeans(n_clusters=600, random_state=0).fit(cars[['ev_x', 'ev_y']])
+    station_location_ixs = kmeans.predict(cars[['ev_x', 'ev_y']])
     station_locations = kmeans.cluster_centers_
-    stations = pd.DataFrame(station_locations, columns=['x_station', 'y_station'])
+    stations = pd.DataFrame(station_locations, columns=['station_x', 'station_y'])
     stations['station_ix'] = np.arange(0, stations.shape[0])
     cars['station_ix'] = station_location_ixs
 
-    ranges = get_ranges(n_sims=number_of_simulations)
-    visit_probs = compute_visiting_probability(ranges)
-    
-    visit_prob_col_names = ['vp{}'.format(i+1) for i in range(number_of_simulations)]
-    visit_probs_table = pd.DataFrame(visit_probs, columns=visit_prob_col_names)
-
-    cars['mean_range'] = np.multiply(ranges, visit_probs).mean(axis=1)
-    cars = pd.concat([cars, visit_probs_table], axis=1)
-
     cars_and_stations = cars.merge(stations, how='left', on='station_ix')
-    cars_and_stations['distance'] = calculate_distance([cars_and_stations['x'], cars_and_stations['y']]
-                                                    , [cars_and_stations['x_station'], cars_and_stations['y_station']])
-    cars_and_stations['driving_cost'] = cars_and_stations['distance'] * driving_cost_per_mile
-    cars_and_stations['charging_cost'] = (250 - cars_and_stations['mean_range'] + cars_and_stations['distance']) * charging_cost_per_mile # * cars_and_stations['visit_prob']
 
-    print(cars_and_stations.head(10))
-
-    range_agg_expression = dict(zip(visit_prob_col_names, ['sum'] * number_of_simulations))
-    agg_expression = {'driving_cost' : 'sum'
-                        , 'mean_range' : 'sum'
-                        , 'charging_cost' : 'sum'
-                        , 'distance' : 'mean'
-                        , 'x_station' : 'mean'
-                        , 'y_station' : 'mean'} | range_agg_expression
-
-    station_stats = cars_and_stations.groupby('station_ix').agg(agg_expression)\
-                                    .reset_index()
-
-    station_stats['n_chargers'] = estimate_number_of_chargers(station_stats[visit_prob_col_names].values)
-
-    total_charging_cost = np.round(station_stats['charging_cost'].sum(), 2)
-    total_driving_cost = np.round(station_stats['driving_cost'].sum(), 2)
-    total_station_cost = np.round(station_stats.shape[0] * construction_cost_per_station, 2)
-    total_maintenance_cost = np.round(station_stats['n_chargers'].sum() * maintenance_fee_per_charger, 2)
-    total_cost = np.round(total_charging_cost + total_driving_cost + total_station_cost + total_maintenance_cost, 2)
-
-    cost_str = "\nCharging cost: ${:,}\nDriving cost: ${:,}\nConstruction cost: ${:,}\nMaintenance cost: ${:,}\n-----------------------------------------------------\nTotal cost: ${:,}.\n"\
-                .format(total_charging_cost, total_driving_cost, total_station_cost, total_maintenance_cost, total_cost)
-
-    print(station_stats.head())
-
-    print(cost_str)
+    costs = compute_cost_per_station(solution=cars_and_stations, n_sims=100)
 
     fig, axs = plt.subplots(figsize=(10,10), ncols=2, nrows=2)
     axs[0,0].scatter(x=cars['x'], y=cars['y'] , c='lightgrey', s=40, label='EV locations')
--- src/utils.py
+++ src/utils.py
@@ -21,3 +21,70 @@ def estimate_number_of_chargers(visit_probabilities:np.array, quant=95):
     percentile = np.percentile(q=quant, a=visit_probabilities, axis=1)
     return np.ceil(percentile / 2)
 
+
+def make_column_names(prefix:str, ncols:int):
+    return ['{}{}'.format(prefix, i+1) for i in range(ncols)]
+
+
+def compute_cost_per_station(solution:pd.DataFrame, n_sims:int=100):
+    """This function calculates the average total costs given a solution and a number of simulations.
+    
+    Keywords:
+        
+        solution (pd.DataFrame) : A table where each row represents an ev and containts its location ['ev_x', 'ev_y'], the 
+            station to which it was assigned ['station_ix'] and the location of said station ['station_x', 'station_y'].
+            
+    Output:
+    
+        aggregated_cost_per_station (pd.DataFrame) : A table where each row describes a station and its cost information.
+
+        """
+
+    driving_cost_per_mile = 0.041
+    charging_cost_per_mile = 0.0388
+    construction_cost_per_station = 5000
+    maintenance_fee_per_charger = 500
+
+    solution['distance'] = calculate_distance([solution['ev_x'], solution['ev_y']]
+                                , [solution['station_x'], solution['station_y']])
+    solution['driving_cost'] = solution['distance'] * driving_cost_per_mile
+
+    visit_prob_col_names = make_column_names(prefix="vp", ncols=n_sims)
+    weighted_range_col_names = make_column_names(prefix="r", ncols=n_sims)
+
+    miles_to_charge = 250 - (get_ranges(n_sims=n_sims) - solution['distance'].values.reshape(-1,1))
+    visit_probs = compute_visiting_probability(miles_to_charge)
+    visit_probs_table = pd.DataFrame(visit_probs, columns=visit_prob_col_names)
+    weighted_range = np.multiply(miles_to_charge, visit_probs)
+    weighted_range_table = pd.DataFrame(weighted_range, columns=weighted_range_col_names)
+
+    solution = pd.concat([solution, visit_probs_table, weighted_range_table], axis=1)
+
+    range_agg_expression = dict(zip(visit_prob_col_names, ['sum'] * n_sims))
+    w_range_agg_expression = dict(zip(weighted_range_col_names, ['sum'] * n_sims))
+    agg_expression = {'driving_cost' : 'sum'
+                        , 'distance' : 'sum'
+                        , 'station_x' : 'mean'
+                        , 'station_y' : 'mean'} | range_agg_expression | w_range_agg_expression
+
+    cost_per_station = solution.groupby('station_ix').agg(agg_expression)\
+                                    .reset_index()
+
+    aggregated_cost_per_station = cost_per_station[['station_ix', 'driving_cost', 'distance', 'station_x', 'station_y']]
+    aggregated_cost_per_station['n_chargers'] = estimate_number_of_chargers(cost_per_station[visit_prob_col_names].values)
+    aggregated_cost_per_station['charging_cost'] = cost_per_station[weighted_range_col_names].values.mean(axis=1) * charging_cost_per_mile
+
+    total_charging_cost = np.round(aggregated_cost_per_station['charging_cost'].sum(), 2)
+    total_driving_cost = np.round(aggregated_cost_per_station['driving_cost'].sum(), 2)
+    total_station_cost = np.round(aggregated_cost_per_station.shape[0] * construction_cost_per_station, 2)
+    total_maintenance_cost = np.round(aggregated_cost_per_station['n_chargers'].sum() * maintenance_fee_per_charger, 2)
+    total_cost = np.round(total_charging_cost + total_driving_cost + total_station_cost + total_maintenance_cost, 2)
+
+    cost_str = "\nCharging cost: ${:,}\nDriving cost: ${:,}\nConstruction cost: ${:,}\nMaintenance cost: ${:,}\n-----------------------------------------------------\nTotal cost: ${:,}.\n"\
+                .format(total_charging_cost, total_driving_cost, total_station_cost, total_maintenance_cost, total_cost)
+
+    print(cost_str)
+
+    return aggregated_cost_per_station
+
+
