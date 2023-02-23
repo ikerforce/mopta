ad9480a52b2de1ba9d6b0f0322feee02a70a9a48
Iker Olarra <iker.mt@gmail.com>
2023-02-23 16:29:57 +0100

    Added a call to the function to solve using PSO. However PSO is still work in progress.

--- src/kcluster_simulation.py
+++ src/kcluster_simulation.py
@@ -1,4 +1,4 @@
-from utils import compute_cost_per_station, solve_with_kmeans, plot_solution, load_ev_locations, solve_with_random_assignment
+from utils import compute_cost_per_station, solve_with_kmeans, plot_solution, load_ev_locations, solve_with_random_assignment, solve_with_pso
 
 
 number_of_simulations = 10
@@ -13,12 +13,13 @@ def main():
 
     ev_locations = load_ev_locations(path="data/MOPTA2023_car_locations.csv")
 
-    ev_and_station_locations = solve_with_random_assignment(ev_locations)
-    # ev_and_station_locations = solve_with_kmeans(ev_locations, n_stations=600)
+    # ev_and_station_locations = solve_with_random_assignment(ev_locations)
+    ev_and_station_locations = solve_with_kmeans(ev_locations, n_stations=600)
+    # ev_and_station_locations = solve_with_pso(ev_locations)
 
-    costs = compute_cost_per_station(solution=ev_and_station_locations, n_sims=100)
+    costs_per_station, total_cost = compute_cost_per_station(solution=ev_and_station_locations, n_sims=100)
 
-    plot_solution(ev_locations, costs)
+    plot_solution(ev_locations, costs_per_station)
 
 if __name__ == '__main__':
     main()
