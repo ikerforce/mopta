02cb563e4e63bbedefb1bfd1503d0b9641a50d72
Iker Olarra <iker.mt@gmail.com>
2023-03-11 16:46:10 +0100

    Changed spacing

--- src/class_implementation.py
+++ src/class_implementation.py
@@ -18,15 +18,18 @@ class ElectricVehicle:
 
 
 class Station:
+
     def __init__(self, x:float, y:float, vehicles:list, construction_cost:float=construction_cost_per_station):
         self.x = x
         self.y = y
         self.vehicles = vehicles
         self.construction_cost = construction_cost
 
+
     def get_number_of_chargers(self):
         visit_probabilities = np.sum([v.visit_prob for v in self.vehicles])
         self.n_chargers = np.ceil(visit_probabilities / 2)
+
 
     def calculate_distances(self):
         ev_xs = [v.x for v in self.vehicles]
@@ -34,17 +37,21 @@ class Station:
         xys = zip(ev_xs, ev_ys)
         self.distances = [calculate_distance([x,y], [self.x, self.y]) for (x,y) in xys]
     
+
     def calculate_driving_cost(self):
         self.calculate_distances()
         self.driving_cost = np.sum(self.distances) * driving_cost_per_mile
+
 
     def calculate_charging_cost(self):
         used_charge = [(250 - v.range) - d for v, d in zip(self.vehicles, self.distances)]
         self.charging_cost = np.sum(used_charge) * charging_cost_per_mile
     
+
     def calculate_maintenance_cost(self):
         self.get_number_of_chargers()
         self.maintenance_cost = compute_adjusted_charger_cost(self.n_chargers)
+
 
     def estimate_station_cost(self):
         self.calculate_driving_cost()
@@ -59,6 +66,7 @@ def generate_random_ev():
     ev = ElectricVehicle(x, y)
     return ev
 
+
 def reassign_vehicle(vehicle_ix:int, previous_station_ix:int, new_station_ix:int, stations:list):
     previous_station = stations[previous_station_ix]
     new_station = stations[new_station_ix]
@@ -68,7 +76,7 @@ def reassign_vehicle(vehicle_ix:int, previous_station_ix:int, new_station_ix:int
     previous_station.estimate_station_cost()
 
 
-n_evs_per_station = [30, 100, 80]
+n_evs_per_station = [10, 40, 50]
 vehicles1 = [generate_random_ev() for i in range(n_evs_per_station[0])]
 vehicles2 = [generate_random_ev() for i in range(n_evs_per_station[1])]
 vehicles3 = [generate_random_ev() for i in range(n_evs_per_station[2])]
@@ -80,8 +88,9 @@ station3 = Station(5,1,vehicles3)
 saved_cost = -1
 iterations = 0
 max_iter = 20
+max_chargers = 8 + 1
 
-while saved_cost < 0 and iterations < max_iter:
+while (saved_cost < 0 or max_chargers > 8) and iterations < max_iter:
 
     iterations+= 1
 
@@ -90,6 +99,8 @@ while saved_cost < 0 and iterations < max_iter:
     for ix, s in enumerate(stations):
         s.estimate_station_cost()
         print('station', ix, s.station_cost)
+
+    max_chargers = np.max([s.n_chargers for s in stations])
 
     initial_total_cost = np.sum(s.station_cost for s in stations)
 
@@ -114,12 +125,13 @@ while saved_cost < 0 and iterations < max_iter:
     cost_increase[max_station_ix] = initial_total_cost
     lowest_increase_ix = np.argmin(cost_increase)
 
+    old_station = stations[max_station_ix]
+    new_station = stations[lowest_increase_ix]
+
     saved_cost = savings + cost_increase[lowest_increase_ix]
-    if saved_cost < 0:
+    if saved_cost < 0 or old_station.n_chargers > new_station.n_chargers:
         print('A vehicle will be relocated from station {} to station {} with a saved expense of {}.'.format(max_station_ix+1, lowest_increase_ix+1, saved_cost))
         reassign_vehicle(vehicle_ix=max_vehicle_ix, previous_station_ix=max_station_ix, new_station_ix=lowest_increase_ix, stations=stations)
-        old_station = stations[max_station_ix]
-        new_station = stations[lowest_increase_ix]
         new_station.vehicles = new_station.vehicles + [most_costly_station_vehicles[max_vehicle_ix]]
         old_station.vehicles = old_station.vehicles[0:max_vehicle_ix] + old_station.vehicles[max_vehicle_ix+1:]
     else:
@@ -137,3 +149,4 @@ for ix, s in enumerate(stations):
     print('n_vehicles: ', len(s.vehicles))
     print('n_chargers:', s.n_chargers)
     print('Cost: ', s.station_cost)
+    print('.-.-.-.-.-.-.-.-.-.')
